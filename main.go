2ae659ec78f194a559e20fb010555ed577c46add
ch0wdreN <i19yanagi@gmail.com>
2025-12-28 01:40:01 +0900

    add explicit flow

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ory/fosite"
+	"github.com/ory/fosite/compose"
+	"github.com/ory/fosite/handler/oauth2"
+	"github.com/ory/fosite/storage"
+	"github.com/ory/fosite/token/jwt"
+)
+
+var (
+	secret = []byte("some-cool-secret-that-is-32bytes")
+	config = &fosite.Config{
+		AccessTokenLifespan: time.Minute * 30,
+		GlobalSecret:        secret,
+	}
+	store      = storage.NewExampleStore()
+	privateKey = mustGenerateKey()
+)
+
+func mustGenerateKey() *rsa.PrivateKey {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	return key
+}
+
+func main() {
+	jwtStrategy := compose.NewOAuth2JWTStrategy(
+		func(_ context.Context) (interface{}, error) {
+			return privateKey, nil
+		},
+		compose.NewOAuth2HMACStrategy(config),
+		config,
+	)
+
+	provider := compose.Compose(
+		config,
+		store,
+		jwtStrategy,
+		compose.OAuth2AuthorizeExplicitFactory,
+		compose.OAuth2RefreshTokenGrantFactory,
+		compose.OAuth2TokenRevocationFactory,
+		compose.OAuth2PKCEFactory,
+	)
+
+	http.HandleFunc("/oauth2/authorization", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		ar, err := provider.NewAuthorizeRequest(ctx, r)
+		if err != nil {
+			log.Printf("Error occurred in NewAuthorizeRequest: %+v", err)
+			provider.WriteAuthorizeError(ctx, w, ar, err)
+			return
+		}
+
+		// Grant all requested scopes
+		for _, scope := range ar.GetRequestedScopes() {
+			ar.GrantScope(scope)
+		}
+
+		mySessionData := &oauth2.JWTSession{
+			JWTClaims: &jwt.JWTClaims{
+				Issuer:    "https://my-oauth-server.com",
+				Subject:   "user-id-123",
+				Audience:  []string{ar.GetClient().GetID()},
+				ExpiresAt: time.Now().Add(time.Hour),
+				IssuedAt:  time.Now(),
+			},
+			JWTHeader: &jwt.Headers{
+				Extra: make(map[string]interface{}),
+			},
+		}
+
+		response, err := provider.NewAuthorizeResponse(ctx, ar, mySessionData)
+		if err != nil {
+			log.Printf("Error occurred in NewAuthorizeResponse: %+v", err)
+			provider.WriteAuthorizeError(ctx, w, ar, err)
+			return
+		}
+
+		provider.WriteAuthorizeResponse(ctx, w, ar, response)
+	})
+
+	http.HandleFunc("/oauth2/token", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		mySessionData := &oauth2.JWTSession{}
+
+		accessRequest, err := provider.NewAccessRequest(ctx, r, mySessionData)
+		if err != nil {
+			log.Printf("Error occurred in NewAccessRequest: %+v", err)
+			provider.WriteAccessError(ctx, w, accessRequest, err)
+			return
+		}
+
+		response, err := provider.NewAccessResponse(ctx, accessRequest)
+		if err != nil {
+			log.Printf("Error occurred in NewAccessResponse: %+v", err)
+			provider.WriteAccessError(ctx, w, accessRequest, err)
+			return
+		}
+
+		provider.WriteAccessResponse(ctx, w, accessRequest, response)
+	})
+
+	http.HandleFunc("/oauth2/revoke", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		err := provider.NewRevocationRequest(ctx, r)
+		if err != nil {
+			log.Printf("Error occurred in NewRevocationRequest: %+v", err)
+			provider.WriteRevocationResponse(ctx, w, err)
+			return
+		}
+
+		provider.WriteRevocationResponse(ctx, w, nil)
+	})
+
+	fmt.Println("OAuth2 server is running on :8080")
+	fmt.Println("  - Authorization endpoint: http://localhost:8080/oauth2/authorization")
+	fmt.Println("  - Token endpoint: http://localhost:8080/oauth2/token")
+	fmt.Println("  - Revoke endpoint: http://localhost:8080/oauth2/revoke")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
